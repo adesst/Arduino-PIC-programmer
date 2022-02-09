6f1167ff77969b77792a75a3c7423600845bd905
adesanto asman <adesantoasman@gmail.com>
2022-02-09 20:07:30 +0700

    PORT only to insert the int:number, instead of /dev/ttyACM0 etc

--- hextoserial/hextoserial.py
+++ hextoserial/hextoserial.py
@@ -1,12 +1,14 @@
-try:
-    import serial
-except:
-    print("Couldn't import serial module. Try installing pyserial library first.")
-    exit(1)
+#!./bin/python
+import serial
 
-port = input("Enter the name of the port: ")
+'''
+This is linux specific
+'''
+port_int = input("Enter PORT number: ")
+port = '/dev/ttyACM%s' %(port_int)
+
 try:
-    ser = serial.Serial(port.strip(" ").capitalize(), 9600)
+    ser = serial.Serial(port, 9600)
 except:
     print("Couldn't open serial communication check the port name.")
     exit(1)
@@ -20,23 +22,44 @@ except:
     exit(1)
 
 hexDict = {}
+
+'''
+reference to: https://microchipdeveloper.com/ipe:sqtp-hex-file-format
+
+For example, consider the line:
+:0300300002337A1E
+
+Breaking this line into its components:
+
+Record Length: 03 (3 bytes of data)
+Address     : 0030 (the 3 bytes will be stored at 0030,
+                    0031, and 0032)
+Record Type : 00 (normal data)
+Data        : 02, 33, 7A
+Checksum    : 1E
+'''
 for line in hexFile:
-    # skipping configuration word and end of file line
     if int(line[3:7], 16) // 2 != 0x2007 and len(line) != 11:
         address = int(line[3:7], 16) // 2
+        if address > 255:
+            continue
         n = int(line[1:3], 16) // 2
         for i in range(n):
             # storing the word in the form of a tuple of two bytes
             hexDict[address] = (bytes.fromhex(line[9 + 4 * i:11 + 4 * i]), bytes.fromhex(line[11 + 4 * i:13 + 4 * i]))
             address += 1
+    else:
+        pass
 
 # filling empty addresses in the middle with un-programmed word 0x3FFF
 for i in range(max(hexDict.keys()) + 1):
     if i not in hexDict.keys():
         hexDict[i] = (b'\xff', b'\x3f')
+
 def main():
     byte = b''
     i = 0
+    decode = ''
     while True:
         if ser.inWaiting() > 0:
             byte = ser.read()
